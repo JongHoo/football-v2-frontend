49a27307d2d1ec964570e5d67f637bbc821bd7b8
= <=>
2021-07-08 21:49:11 +0900

    layout 사용하지 않는 login 페이지 추가

---
+++ pages/login.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  #login
+    .form-wrapper
+      input(type="text")
+      input(type="password")
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
